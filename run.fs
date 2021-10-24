937ebafa20c4c4ae0319d9411854d7ab637df6a7
NicoVIII <nico@nicoviii.net>
2021-10-24 08:43:43 +0000

    Move publishing in run script

--- run.fs
+++ run.fs
@@ -41,18 +41,20 @@ type JobBuilder() =
     member __.Run f = f ()
 
     member __.Yield x = x
+    member __.Zero() = Ok
 
 let job = JobBuilder()
 
 [<RequireQualifiedAccess>]
 module Config =
-    let dataFolder = "data"
+    let dataFolder = "./data"
 
     let mainProject =
-        "src/MagicCollectionHelper/AvaloniaApp/MagicCollectionHelper.AvaloniaApp.fsproj"
+        "./src/MagicCollectionHelper/AvaloniaApp/MagicCollectionHelper.AvaloniaApp.fsproj"
 
-    let testFolder = "tests"
+    let testFolder = "./tests"
 
+    let packPath = "./deploy"
 
 module Task =
     let restore () =
@@ -110,6 +112,35 @@ module Task =
 
             Ok
 
+    let publish () =
+        let commonArgs =
+            [ "-v"; "minimal"
+              "-c"; "Release"
+              "-o"; Config.packPath
+              "/p:SelfContained=true"
+              "/p:PublishSingleFile=true"
+              "/p:PublishTrimmed=true"
+              "/p:TrimMode=Link"
+              "/p:IncludeNativeLibrariesForSelfExtract=true"
+              "/p:DebugType=None";
+              Config.mainProject ]
+
+        Shell.rm Config.packPath
+        Shell.mkdir Config.packPath
+
+        job {
+            // Linux
+            dotnet [ "publish"; "-r"; "linux-x64"; "/p:PublishReadyToRun=true"; yield! commonArgs ]
+            Shell.mv $"{Config.packPath}/MagicCollectionHelper.AvaloniaApp" $"{Config.packPath}/MagicCollectionHelper-linux-x64"
+
+            // Windows
+            dotnet [ "publish"; "-r"; "win-x64"; yield! commonArgs ]
+            Shell.mv $"{Config.packPath}/MagicCollectionHelper.AvaloniaApp.exe" $"{Config.packPath}/MagicCollectionHelper-win-x64.exe"
+
+            // macOS single-file publishing is not working, there are dylibs generated while publishing for some reason
+            //dotnet [ "publish"; "-r"; "osx-x64"; yield! commonArgs ]
+        }
+
 module Command =
     let restore () = Task.restore ()
 
@@ -131,6 +162,12 @@ module Command =
             Task.test ()
         }
 
+    let publish () =
+        job {
+            restore ()
+            Task.publish ()
+        }
+
     let setupTestdata () = Task.setupTestdata ()
 
 [<EntryPoint>]
@@ -143,6 +180,7 @@ let main args =
         | []
         | [ "run" ] -> Command.run ()
         | [ "test" ] -> Command.test ()
+        | [ "publish" ] -> Command.publish ()
         | [ "setup-testdata" ] -> Command.setupTestdata ()
         | _ ->
             printfn "Usage: dotnet run [<command>]"
--- scripts/ressources/config.fsx
+++
@@ -1,8 +0,0 @@
-/// Contains config options for bundling script
-module Config =
-    let outputPath = "./deploy"
-
-    let projectPath =
-        "./src/MagicCollectionHelper/AvaloniaApp/MagicCollectionHelper.AvaloniaApp.fsproj"
-
-    let testsBasePath = "./tests/MagicCollectionHelper/Tests/"
